4f4b17b2df6d4800d5a557395dbc892b0a4c15a2
Wende Tan <twd2@163.com>
2016-06-26 19:39:23 +0800

    system info

--- smartd/view/main.py
+++ smartd/view/main.py
@@ -1,19 +1,37 @@
+import os
+import psutil
 from smartd import app
 from smartd.view import base
 from smartd.model import data
+from smartd.util import format
 from tornado import web, gen, options
 
-def size_format(num, suffix='B'):
-  for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
-    if abs(num) < 1024.0:
-      return "%3.1f%s%s" % (num, unit, suffix)
-    num /= 1024.0
-  return "%.1f%s%s" % (num, 'Yi', suffix)
+def get_cpu_info():
+  cpu_count = psutil.cpu_count()
+  return {'count': cpu_count,
+          'load': os.getloadavg(),
+          'percent': os.getloadavg()[0] * 100 / cpu_count}
+
+def get_mem_info():
+  mem_info = psutil.virtual_memory()
+  return {'total': mem_info.total, 'available': mem_info.available, 'percent': mem_info.percent}
+
+def get_net_info():
+  net_info = psutil.net_io_counters()
+  return {'sent': net_info.bytes_sent, 'recv': net_info.bytes_recv}
 
 @app.route('Home', '/')
 class MainHandler(base.BaseHandler):
   @gen.coroutine
   def get(self):
-    self.render('main.html', count=(yield data.count()), size=(yield data.total_size()),
-                format=size_format)
+    cpu_info = get_cpu_info()
+    mem_info = get_mem_info()
+    net_info = get_net_info()
 
+    count = yield data.count()
+    if count > 0:
+      size = yield data.total_size()
+    else:
+      size = 0
+    self.render('main.html', count=count, size=size, format=format.size_format,
+                cpu_info=cpu_info, mem_info=mem_info, net_info=net_info)
